e918b71e1c5c2d5fac45734cfc64b7429f745fa6
Craig Clayton <craig__clayton@outlook.com>
2025-03-22 16:44:00 +0000

    add metadata to contact me page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contact Me',
+  description:
+    'Passionate front-end developer with a focus on building fast, modern, and accessible web applications.',
+};
+
 const ContactPage = () => {
   return <div>ContactPage</div>;
 };
