d334738c74acc77a203c3611c0adbf33116aff2b
Craig Clayton <craig__clayton@outlook.com>
2025-03-22 16:40:45 +0000

    add links in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
-    <header>
-      <p>Craig Clayton</p>
+    <header className='px-4 py-4 flex justify-between items-center'>
+      <p className='text-gray-400'>Craig Clayton</p>
+      <div>
+        <Link href='/about'>About Me</Link>
+        <Link href='/contact'>Contact Me</Link>
+      </div>
     </header>
   );
 };
